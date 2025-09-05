d5aef4ac1494b0aab990da90267329e5281314f4
Lucas Schneider <lucasmoraes1910@gmail.com>
2025-09-05 00:52:47 -0300

    galeria-imagens

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar Itens do Orçamento
-
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -25,7 +24,6 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
-
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
@@ -43,3 +41,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
